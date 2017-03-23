949bed3ab69aff2fb34ec72091544831a8a93fd1
root <root@33f624d9fc72>
2017-03-23 11:00:32 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "TestLoginApp",
+  "homePage" : "Main",
+  "name" : "TestLoginApp",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
