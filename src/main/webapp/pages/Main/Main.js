97276e7d67b64a62658d4e31e6adbf35eb033103
Bijesh Chandran <bijesh@vanenburgsoftware.com>
2017-04-11 03:34:48 +0000

    First checkin New files added - [hrdb,app,HoursAccOnt,prefabs,Login1,Employeeprofile,intercept-urls.json,hrdb.properties,hrdb.log,hrdb.script,roles.json] Files modified - [demo-options.json,pages-config.json,Main.html,footer.variables.json,rightnav.variables.json,project-services.xml,Common.variables.json,deployment.properties,securityService.properties,Login.variables.json,app.variables.json,topnav.variables.json,Main.variables.json,leftnav.variables.json,header.variables.json,Main.js,general-options.json,project-security.xml,pom.xml,development.properties]

--- src/main/webapp/pages/Main/Main.js
+++ src/main/webapp/pages/Main/Main.js
@@ -7,7 +7,7 @@ Application.$controller("MainPageController", ["$scope", function ($scope) {
          * variables can be accessed through '$scope.Variables' property here
          * e.g. to get dataSet in a staticVariable named 'loggedInUser' use following script
          * $scope.Variables.loggedInUser.getData()
-         * 
+         *
          * widgets can be accessed through '$scope.Widgets' property here
          * e.g. to get value of text widget named 'username' use following script
          * '$scope.Widgets.username.datavalue'
@@ -15,3 +15,18 @@ Application.$controller("MainPageController", ["$scope", function ($scope) {
     };
 
 }]);
+
+
+Application.$controller("grid1Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
+
+Application.$controller("liveform1Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
